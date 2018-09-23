e5a96c25dafae8c7952fef952d0f2016dbcd19b1
Omar Flores Grimontt <omar.flores@3dves.com>
2018-09-22 21:29:11 -0500

    Integrated postcss with the project

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Main from './main';
 import registerServiceWorker from './js/services/registerServiceWorker';
-import './css/main.css';
+import './index.css';
 
 ReactDOM.render(<Main />, document.getElementById('root'));
 registerServiceWorker();
