d892313b653e5301e6b36e242b13ea7e7a71d299
Omar Flores Grimontt <omar.flores@3dves.com>
2018-09-24 19:59:10 -0500

    created containers for main section

---
+++ src/components/container/section-container.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const SectionContainer = (props) => {
+    return (
+        <div>
+            {props.children}
+        </div>
+    );
+};
+
+export default SectionContainer;
